a2aabce75af1ddca4a4eeed742691bc8bf06619b
Araiva <kizzaraiva@gmail.com>
2024-02-18 15:09:16 +0700

    feat: user module add feature and user module.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FeaturesModule } from './features/features.module';
 
 @Module({
-  imports: [],
+  imports: [
+    FeaturesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
