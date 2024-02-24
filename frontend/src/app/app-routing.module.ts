4cce80a7324d03456ceb5cccc0c64ea21e99499a
Araiva <kizzaraiva@gmail.com>
2024-02-24 23:52:31 +0700

    routing setup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {PageNotFoundComponent} from './shared/components/page-not-found/page-not-found.component';
 
-const appRoutes: Routes = [
-  {path: '', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)},
-  // {path: '**', component}
+const routes: Routes = [
+  {path: 'main', loadChildren: () => import('./modules/main/main.module').then(m => m.MainModule)},
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
 ]
 @NgModule({
-  imports: [],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {RouterOutlet} from '@angular/router';
+import {SharedModule} from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +12,7 @@ import {RouterOutlet} from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterOutlet
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
