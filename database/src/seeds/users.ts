a6136d61f2eadee72fc0e65e9516decb84935569
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-05-04 20:06:05 +0700

    feat: manual user seed

--- database/src/seeds/users.ts
+++ database/src/seeds/users.ts
@@ -1,7 +1,61 @@
 import { User } from '@entities/user.entity';
+import * as argon2 from 'argon2'
 
-const userSeed: Partial<User>[] = [
-
+const getUserSeed = async (): Promise<Partial<User>[]> => [
+  {
+    id: 1,
+    username: 'araiva',
+    password: await argon2.hash('123456'),
+    firstName: 'Araiva',
+    lastName: 'Aviara',
+    email: 'araiva@gmail.com',
+    imageUrl: 'https://avatarfiles.alphacoders.com/825/82567.jpg',
+  },
+  {
+    id: 2,
+    username: 'viruskizz',
+    password: await argon2.hash('123456'),
+    firstName: 'Viruskizz',
+    lastName: 'Zziksuriv',
+    email: 'viruskizz@gmail.com',
+    imageUrl: 'https://avatarfiles.alphacoders.com/358/358011.jpg',
+  },
+  {
+    id: 3,
+    username: 'aporlo',
+    password: await argon2.hash('123456'),
+    firstName: 'Aporlo',
+    lastName: 'Olropa',
+    email: 'aporlo@gmail.com',
+    imageUrl: 'https://avatarfiles.alphacoders.com/926/thumb-1920-92694.gif',
+  },
+  {
+    id: 4,
+    username: 'kthitiwut',
+    password: await argon2.hash('123456'),
+    firstName: 'Thitiwut',
+    lastName: 'Somsa',
+    email: 'kthitiwut@gmail.com',
+    imageUrl: 'https://avatarfiles.alphacoders.com/353/353809.png',
+  },
+  {
+    id: 5,
+    username: 'abcde',
+    password: await argon2.hash('123456'),
+    firstName: 'Abcde',
+    lastName: 'Xyz',
+    email: 'abcde@gmail.com',
+    imageUrl: 'https://avatarfiles.alphacoders.com/926/thumb-1920-92694.gif',
+  },
+  {
+    id: 6,
+    username: 'xyz',
+    password: await argon2.hash('123456'),
+    firstName: 'Xyz',
+    lastName: 'Abc',
+    email: 'xyz@gmail.com',
+    imageUrl: 'https://avatarfiles.alphacoders.com/372/thumb-1920-372992.jpg',
+  }
 ];
 
-export default userSeed;
+export default getUserSeed;
