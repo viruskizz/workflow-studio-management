fee9cc56b97736c37654baa6f37f2b71018b2b3c
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-06-30 17:02:51 +0700

    feat: backend auth expireIn 24h

--- backend/src/features/auth/auth.module.ts
+++ backend/src/features/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { jwtConstants } from '@backend/features/auth/strategies/jwt/constants';
     PassportModule,
     JwtModule.register({
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '24h' },
     }),
   ],
   controllers: [AuthController],
