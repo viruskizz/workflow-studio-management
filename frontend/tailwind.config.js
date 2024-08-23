7800fea5c08d4b8494b85f322c2a295c4be53586
Mok <mokmaard646@gmail.com>
2024-08-23 09:57:14 +0700

    UI

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors : {
+        "search-box" : 'rgb(165,	165,	173)',
+        "purple-button" : 'rgb(75,	49,	220	)',
+        "table-bg" : 'rgb(245,	245,	245)',
+        "thead" : 'rgb(241,	242,	243)',
+        "table-border" : 'rgb(241	242	243)',
+        "orange-line" : 'rgb(242 169	59)'
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
