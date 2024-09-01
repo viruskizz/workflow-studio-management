1bd9d4dc0f5b393fa6fef9d6dd09dfaedcd15bd8
Mok <mokmaard646@gmail.com>
2024-09-01 16:22:00 +0700

    team interface

---
+++ frontend/src/app/models/team.model.ts
@@ -0,0 +1,9 @@
+export interface TeamMember {
+  teamLogo : string;
+  teamName : string;
+  key : string;
+  description : string;
+  leaderPic : string;
+  memberPics : string[];
+  projectName : string;
+}
