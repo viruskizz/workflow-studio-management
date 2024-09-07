eaccb03c45317c778af6fdae28b6c118169df2cc
atichat <atichatfantasy@gmail.com>
2024-09-07 20:57:33 +0700

    feat(project): add dynamic table

--- frontend/src/app/modules/projects/projects.module.ts
+++ frontend/src/app/modules/projects/projects.module.ts
@@ -4,7 +4,6 @@ import { ProjectsComponent } from './projects.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { DynamicTableComponent } from './dynamic-table/dynamic-table.component';
-import { BrowserModule } from "@angular/platform-browser";
 
 const routes: Routes = [
   { path: '', component: ProjectsComponent }
