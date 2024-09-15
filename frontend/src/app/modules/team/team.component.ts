2005e63e310aede4e4fb2652b09ba034e06829f3
Mok <mokmaard646@gmail.com>
2024-09-15 13:48:53 +0700

    mapping content into team, prepare for fetching data.

--- frontend/src/app/modules/team/team.component.ts
+++ frontend/src/app/modules/team/team.component.ts
@@ -8,33 +8,39 @@ import { DynamicTableComponent, TableHeader } from '../../shared/components/dyna
   templateUrl: './team.component.html',
 })
 export class TeamComponent implements AfterViewInit {
-  datas = [1,2,3,4];
-
   teamsCount = 10;
-  tableHeader = "Teams";
+  tableHeader = 'Teams';
 
-  teamRows : TeamRow[] = [];
+  teamRows: TeamRow[] = [];
 
-  @ViewChild('dyntable') table! : DynamicTableComponent;
+  @ViewChild('dyntable') table!: DynamicTableComponent;
 
-  key = "KEY";
-  description = "description";
-  currentProject = "currentProject";
-
-  iconOutline = "outline outline-4 outline-white rounded-full";
-
-  isHover = false;
-  isSVGhover = false;
-
-  tableDataPadding = "px-4 py-4"
-
+  key = 'KEY';
+  description = 'description';
+  currentProject = 'currentProject';
   //icons
-  teamIcon = "https://masteringruneterra.com/wp-content/plugins/deck-viewer/assets/images/champions/01PZ008.webp";
-  leaderIcon = "https://64.media.tumblr.com/4af2b4026f724a749b27e4cbae3f914a/d2464c1d38422cb8-d8/s1280x1920/1873c68cbe57ab93ef97412adb1d715082e73bc6.jpg";
+  teamIcon =
+    'https://masteringruneterra.com/wp-content/plugins/deck-viewer/assets/images/champions/01PZ008.webp';
+  leaderIcon =
+    'https://64.media.tumblr.com/4af2b4026f724a749b27e4cbae3f914a/d2464c1d38422cb8-d8/s1280x1920/1873c68cbe57ab93ef97412adb1d715082e73bc6.jpg';
+  memberIcon =
+    'https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp';
 
-  teamName = "Delta Force 510"
+  teamName = 'Delta Force 510';
 
-  constructor(private TeamsService : TeamsService ) {}
+  datas = Array.from({length : 4}, () => {
+    return {
+      teamIcon: this.teamIcon,
+      teamName: this.teamName,
+      key: this.key,
+      description: this.description,
+      leaderIcon: this.leaderIcon,
+      membersIcon: Array.from({ length: 20 }, () => this.memberIcon),
+      currentProject: this.currentProject,
+    };
+  });
+
+  constructor(private TeamsService: TeamsService) {}
 
   ngAfterViewInit(): void {
     const headers: TableHeader[] = [
@@ -56,5 +62,4 @@ export class TeamComponent implements AfterViewInit {
   //     console.log( this.teamRows );
   //   });
   // }
-
 }
