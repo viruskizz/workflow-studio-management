0aea93e000723fed4f22f85282db28e942bb4cf3
viruskizz <kizzaraiva@gmail.com>
2024-10-09 21:19:59 +0700

    refact: remove non use component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,7 +1,5 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { RouterModule } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DynamicTableComponent } from './components/dynamic-table/dynamic-table.component';
@@ -32,8 +30,6 @@ const svgs: any[] = [
 ]
 
 const sharedComponent: any[] = [
-  HeaderComponent,
-  SidebarComponent,
   DynamicTableComponent,
   TableHeaderComponent,
   IconComponent
