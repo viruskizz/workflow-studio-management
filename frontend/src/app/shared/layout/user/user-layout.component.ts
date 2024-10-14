f53c70a9c99d6c171e928db940b5ba45bc7a4233
viruskizz <kizzaraiva@gmail.com>
2024-10-14 19:54:10 +0700

    50 reproduce all process (#51)
    * re create sidebar
    
    * move all layout
    
    * refact: remove non use component

--- frontend/src/app/shared/layout/user/user-layout.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-user-layout',
-  templateUrl: './user-layout.component.html',
-  // styleUrl: './app.component.css'
-})
-export class UserLayoutComponent {}
