a8bc4da7b9b88a7909603c403f0e1eac44cf2854
Mok <mokmaard646@gmail.com>
2024-10-17 15:20:45 +0700

    feat(stat-box): create stat-box component.

---
+++ frontend/src/app/modules/user-detail/user-profile/work-stat/work-stat.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-work-stat',
+  templateUrl: './work-stat.component.html',
+  styleUrl: './work-stat.component.css',
+})
+export class WorkStatComponent {
+}
