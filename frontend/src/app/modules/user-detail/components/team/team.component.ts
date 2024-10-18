40b0ab464e5ed95e1d53b7bf542e2d2eaad4081b
Mok <mokmaard646@gmail.com>
2024-10-18 22:04:27 +0700

    refactor(team): rename selector to user-team.

--- frontend/src/app/modules/user-detail/components/team/team.component.ts
+++ frontend/src/app/modules/user-detail/components/team/team.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-team',
+  selector: 'app-user-team',
   templateUrl: './team.component.html',
   styleUrl: './team.component.css',
 })
