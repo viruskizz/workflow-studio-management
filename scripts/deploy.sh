8da391fbf6db048bb258154042d4a6e72d9eae0f
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-20 23:37:06 +0700

    fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,10 @@
 APP_NAME=workflow
 APP_GROUP=backend
 REPO=viruskizz/workflow-studio-management
-COMMIT_ID=229c7888c16fe3daa84147d0f4a0dd8f68f942d9
+COMMIT_ID=b46b9fef1819ce60a3dca031c57f2585831b24a1
 aws deploy create-deployment \
   --application-name $APP_NAME \
   --deployment-config-name CodeDeployDefault.OneAtATime \
   --deployment-group-name $APP_GROUP \
-  --file-exists-behavior OVERWRITE\
   --description "My GitHub deployment demo" \
   --github-location repository=$REPO,commitId=$COMMIT_ID
