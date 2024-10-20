0bc948b9296815e508ff747bb99c4bed82ee2e3e
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-20 22:37:31 +0700

    update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,10 @@
+APP_NAME=workflow
+APP_GROUP=workflow-backend
+REPO=viruskizz/workflow-studio-management
+COMMIT_ID=e0ea3d0128c1b26d5bdf28440d66087b7de71f3b
 aws deploy create-deployment \
-  --application-name CodeDeployGitHubDemo-App \
+  --application-name $APP_NAME \
   --deployment-config-name CodeDeployDefault.OneAtATime \
-  --deployment-group-name CodeDeployGitHubDemo-DepGrp \
+  --deployment-group-name $APP_GROUP \
   --description "My GitHub deployment demo" \
-  --github-location repository=repository,commitId=commit-id
+  --github-location repository=$REPO,commitId=$COMMIT_ID
