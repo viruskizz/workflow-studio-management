b46b9fef1819ce60a3dca031c57f2585831b24a1
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-20 23:31:28 +0700

    fix: app stop name

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,11 @@
 APP_NAME=workflow
-APP_GROUP=workflow-backend
+APP_GROUP=backend
 REPO=viruskizz/workflow-studio-management
-COMMIT_ID=e0ea3d0128c1b26d5bdf28440d66087b7de71f3b
+COMMIT_ID=229c7888c16fe3daa84147d0f4a0dd8f68f942d9
 aws deploy create-deployment \
   --application-name $APP_NAME \
   --deployment-config-name CodeDeployDefault.OneAtATime \
   --deployment-group-name $APP_GROUP \
+  --file-exists-behavior OVERWRITE\
   --description "My GitHub deployment demo" \
   --github-location repository=$REPO,commitId=$COMMIT_ID
