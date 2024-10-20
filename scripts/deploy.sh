963b598b34441c3ea662c9715839d37a7c440b9c
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-21 00:50:45 +0700

    ci: test deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 APP_NAME=workflow
 APP_GROUP=backend
 REPO=viruskizz/workflow-studio-management
-COMMIT_ID=3b78ea3e5bd10d0af625148e5f8c4ac406416117
+COMMIT_ID=$1
 aws deploy create-deployment \
   --application-name $APP_NAME \
   --deployment-config-name CodeDeployDefault.OneAtATime \
