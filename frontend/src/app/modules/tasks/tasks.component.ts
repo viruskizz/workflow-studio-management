43fc56643cf99aeb0c317fd2012bdd25ad040fea
Thitiwut Somsa <kizzaraiva@gmail.com>
2024-10-28 16:41:07 +0700

    copied from user

--- frontend/src/app/modules/tasks/tasks.component.ts
+++ frontend/src/app/modules/tasks/tasks.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { User } from '../../models/user.model';
+import { PeriodicElement } from '../users/users.component';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-taks',
   templateUrl: './tasks.component.html'
 })
-export class TasksComponent {
-    
+export class TasksComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'avatar', 'username', 'firstname', 'lastname', 'actions'];
+  dataSource = new MatTableDataSource<User>([]);
+  clickedRows = new Set<PeriodicElement>();
+  searchKeywordFilter = new FormControl();
+
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.listUser().subscribe((users) => {
+      this.dataSource.sort = this.sort;
+      this.dataSource.data = users;
+    });
+  }
+
+  applyFilter(event: any) {
+    const filterValue = (event.target as HTMLInputElement).value
+    const tableFilters = [];
+    tableFilters.push({
+      id: 'name',
+      value: filterValue
+    });
+    // this.dataSource.filter = JSON.stringify(tableFilters);
+    this.dataSource.filter = filterValue.toLowerCase()
+  }
 }
--- frontend/src/app/modules/tasks/tasks.module.ts
+++ frontend/src/app/modules/tasks/tasks.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from "@angular/core";
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { TasksComponent } from "./tasks.component";
 import { SharedModule } from "../../shared/shared.module";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatTableModule } from "@angular/material/table";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 const routes: Routes = [
   { path: '', component: TasksComponent }
@@ -18,6 +23,12 @@ const routes: Routes = [
     SharedModule,
     RouterModule.forChild(routes),
     ReactiveFormsModule,
+    DatePipe,
+    FontAwesomeModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
   ]
 })
 export class TasksModule { }
