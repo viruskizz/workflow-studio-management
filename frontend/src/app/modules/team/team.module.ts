aebc4555d7513898353fb04e1c7558069ea90ee7
atichat <atichatfantasy@gmail.com>
2024-11-06 16:08:53 +0700

    feat(Team): update Team table with Material Table

--- frontend/src/app/modules/team/team.module.ts
+++ frontend/src/app/modules/team/team.module.ts
@@ -1,27 +1,31 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { TeamComponent } from './team.component';
 import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from "../../shared/shared.module";
-import { CommonModule } from "@angular/common";
+import { SharedModule } from '../../shared/shared.module';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 import { TeamTdComponent } from './components/team-td/team-td.component';
 import { MemberIconComponent } from './components/member-icon/member-icon.component';
 
-const routes: Routes = [
-  { path: '', component: TeamComponent }
-]
+const routes: Routes = [{ path: '', component: TeamComponent }];
 
 @NgModule({
-  declarations: [
-    TeamComponent,
-    TeamTdComponent,
-    MemberIconComponent,
-  ],
+  declarations: [TeamComponent, TeamTdComponent, MemberIconComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     ReactiveFormsModule,
-    SharedModule
-  ]
+    SharedModule,
+    MatTableModule,
+    MatSortModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
 })
-export class TeamModule { }
+export class TeamModule {}
