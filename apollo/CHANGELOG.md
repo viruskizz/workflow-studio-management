a99383a0907a72fa869fdc270562876536a522ed
viruskizz <kizzaraiva@gmail.com>
2024-11-13 21:45:31 +0700

    feat: add prime template

---
+++ apollo/CHANGELOG.md
@@ -0,0 +1,56 @@
+# Changelog
+
+# 17.0.0
+**Implemented New Features and Enhancements:**
+- Upgrade to PrimeNG 17
+- Upgrade to Angular 17
+
+# 16.0.0
+- Upgrade to PrimeNG 16
+- Upgrade to Angular 16
+- Update theme files
+
+# 15.1.0
+
+- General style adjustments
+
+**Implemented New Features and Enhancements:**
+- New Drawer menu
+- Horizontal & Slim & SlimPlus menu scroll
+
+# 15.0.1
+**Fixed bugs:**
+- Fixed an issue where checkbox does not reflect the actual value of task list in kanban sidebar.
+
+## 15.0.0
+**Migration Guide**
+- Upgrade to PrimeNG 15
+- Upgrade to Angular 15
+  
+**Implemented New Features and Enhancements:**
+- Update to PrimeNG 15
+- Update to Angular 15
+
+## 14.0.3
+- Tooltips for slim menu
+
+## 14.0.2
+- Updated UI kit demos
+- Updated PrimeNG
+- Enabled lint
+
+## 14.0.1
+- Refactored app.layout.component.ts
+- Fixed issue where double click is necessary to hide a kanban card overlay menu.
+
+## 14.0.0
+**Migration Guide**
+- Upgrade to PrimeNG 14
+- Upgrade to Angular 14
+  
+**Implemented New Features and Enhancements:**
+- Update to PrimeNG 14
+- Update to Angular 14
+
+## 13.2.0
+- Remastered from the ground up with a new design
---
+++ apollo/src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { AppLayoutComponent } from './layout/app.layout.component';
+
+const routerOptions: ExtraOptions = {
+    anchorScrolling: 'enabled'
+};
+
+const routes: Routes = [
+    {
+        path: '', component: AppLayoutComponent,
+        children: [
+            { path: '', loadChildren: () => import('./modules/dashboards/dashboards.module').then(m => m.DashboardsModule) },
+            // Template Apollo
+            { path: 'dashboard', loadChildren: () => import('./demo/components/dashboards/dashboards.module').then(m => m.DashboardsModule) },
+            { path: 'uikit', data: { breadcrumb: 'UI Kit' }, loadChildren: () => import('./demo/components/uikit/uikit.module').then(m => m.UIkitModule) },
+            { path: 'utilities', data: { breadcrumb: 'Utilities' }, loadChildren: () => import('./demo/components/utilities/utilities.module').then(m => m.UtilitiesModule) },
+            { path: 'pages', data: { breadcrumb: 'Pages' }, loadChildren: () => import('./demo/components/pages/pages.module').then(m => m.PagesModule) },
+            { path: 'profile', data: { breadcrumb: 'User Management' }, loadChildren: () => import('./demo/components/profile/profile.module').then(m => m.ProfileModule) },
+            { path: 'documentation', data: { breadcrumb: 'Documentation' }, loadChildren: () => import('./demo/components/documentation/documentation.module').then(m => m.DocumentationModule) },
+            { path: 'blocks', data: { breadcrumb: 'Prime Blocks' }, loadChildren: () => import('./demo/components/primeblocks/primeblocks.module').then(m => m.PrimeBlocksModule) },
+            { path: 'ecommerce', data: { breadcrumb: 'E-Commerce' }, loadChildren: () => import('./demo/components/ecommerce/ecommerce.module').then(m => m.EcommerceModule) },
+            { path: 'apps', data: { breadcrumb: 'Apps' }, loadChildren: () => import('./demo/components/apps/apps.module').then(m => m.AppsModule) }
+        ]
+    },
+    { path: 'auth', data: { breadcrumb: 'Auth' }, loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+    // { path: 'landing', loadChildren: () => import('./demo/components/landing/landing.module').then(m => m.LandingModule) },
+    { path: 'notfound', loadChildren: () => import('./modules/notfound/notfound.module').then(m => m.NotfoundModule) },
+    { path: '**', redirectTo: '/notfound' }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, routerOptions)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
