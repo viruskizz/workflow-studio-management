29c900264cac38dcee4fc8e19b15ad953849130f
viruskizz <kizzaraiva@gmail.com>
2025-01-17 10:17:13 +0700

    100 frontend project table view (#103)
    * feat: copied from demo
    
    * feat: add tree able

--- apollo/src/app/modules/project/project-routing.module.ts
+++ apollo/src/app/modules/project/project-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProjectComponent } from './project.component';
+import { ProjectViewComponent } from './project-view/project-view.component';
 
 @NgModule({
 	imports: [RouterModule.forChild([
-		{ path: '', component: ProjectComponent }
+		{ path: '', component: ProjectComponent },
+		{ path: ':id', component: ProjectViewComponent },
 	])],
 	exports: [RouterModule]
 })
