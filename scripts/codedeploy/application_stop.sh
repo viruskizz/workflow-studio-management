9767bad68dafc9fea5966348e71ff773dafa7cb7
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-04-12 01:32:31 +0700

    fix pm22

--- scripts/codedeploy/application_stop.sh
+++ scripts/codedeploy/application_stop.sh
@@ -2,11 +2,13 @@
 source $(dirname $0)/variable.sh
 
 echo "PM2_FRONTEND_NAME=$PM2_FRONTEND_NAME"
-if [ -n "$(pm2 pid $PM2_FRONTEND_NAME)" ]; then
+if [ ! -z "$(pm2 pid $PM2_FRONTEND_NAME)" ]; then
+    pm2 pid $PM2_FRONTEND_NAME
     pm2 delete $PM2_FRONTEND_NAME
 fi
 
 echo "PM2_BACKEND_NAME=$PM2_BACKEND_NAME"
-if [ -n "$(pm2 pid $PM2_BACKEND_NAME)" ]; then
+if [ ! -z "$(pm2 pid $PM2_BACKEND_NAME)" ]; then
+    pm2 pid $PM2_BACKEND_NAME
     pm2 delete $PM2_BACKEND_NAME
 fi
