fe3e5676927a6055853a5c7dda16cdaa611a23f1
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-04-12 01:41:42 +0700

    remove stop

--- scripts/codedeploy/application_stop.sh
+++ scripts/codedeploy/application_stop.sh
@@ -3,12 +3,12 @@ source $(dirname $0)/variable.sh
 
 set -e
 
-echo "PM2_FRONTEND_NAME=$PM2_FRONTEND_NAME"
-if [ ! -z "$(pm2 pid $PM2_FRONTEND_NAME)" ]; then
-    pm2 delete $PM2_FRONTEND_NAME || true
-fi
+# echo "PM2_FRONTEND_NAME=$PM2_FRONTEND_NAME"
+# if [ ! -z "$(pm2 pid $PM2_FRONTEND_NAME)" ]; then
+#     pm2 delete $PM2_FRONTEND_NAME || true
+# fi
 
-echo "PM2_BACKEND_NAME=$PM2_BACKEND_NAME"
-if [ ! -z "$(pm2 pid $PM2_BACKEND_NAME)" ]; then
-    pm2 delete $PM2_BACKEND_NAME || true
-fi
+# echo "PM2_BACKEND_NAME=$PM2_BACKEND_NAME"
+# if [ ! -z "$(pm2 pid $PM2_BACKEND_NAME)" ]; then
+#     pm2 delete $PM2_BACKEND_NAME || true
+# fi
