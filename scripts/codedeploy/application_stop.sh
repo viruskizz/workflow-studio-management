b3f81d7b27c8c498a6833c22f353ae8758814948
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-04-12 01:36:45 +0700

    ignore pm2 error

--- scripts/codedeploy/application_stop.sh
+++ scripts/codedeploy/application_stop.sh
@@ -3,12 +3,10 @@ source $(dirname $0)/variable.sh
 
 echo "PM2_FRONTEND_NAME=$PM2_FRONTEND_NAME"
 if [ ! -z "$(pm2 pid $PM2_FRONTEND_NAME)" ]; then
-    pm2 pid $PM2_FRONTEND_NAME
-    pm2 delete $PM2_FRONTEND_NAME
+    pm2 delete $PM2_FRONTEND_NAME || true
 fi
 
 echo "PM2_BACKEND_NAME=$PM2_BACKEND_NAME"
 if [ ! -z "$(pm2 pid $PM2_BACKEND_NAME)" ]; then
-    pm2 pid $PM2_BACKEND_NAME
-    pm2 delete $PM2_BACKEND_NAME
+    pm2 delete $PM2_BACKEND_NAME || true
 fi
