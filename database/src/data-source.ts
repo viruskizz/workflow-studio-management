d645f786652b299189add7c63fafe1471438bee8
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-04-11 23:43:50 +0700

    prepare prod

--- database/src/data-source.ts
+++ database/src/data-source.ts
@@ -5,12 +5,18 @@ import entities from '@entities/index'
 dotenv.config({ path: '../.env' })
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: "mysql",
+  // host: "localhost",
+  // port: +process.env.DB_PORT,
+  // username: process.env.DB_USERNAME,
+  // password: process.env.DB_PASSWORD,
+  // database: process.env.DB_NAME,
   host: "localhost",
-  port: +process.env.DB_PORT,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  port: +process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+
   synchronize: true,
   logging: false,
   entities: entities,
