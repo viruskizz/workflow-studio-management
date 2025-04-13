332c792deb24399835e370593e65bcbd8328a3a2
Thitiwut Somsa <kizzaraiva@gmail.com>
2025-04-13 19:23:31 +0700

    refact: script

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 sudo apt update
 curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -
-sudo apt install -y nodejs git postgresql
+sudo apt install -y nodejs git vim
 
 # install code
 git clone \
     -b main \
     https://github.com/viruskizz/workflow-studio-management.git
 
+# Install MySQL
+wget https://dev.mysql.com/get/mysql-apt-config_0.8.33-1_all.deb
+sudo dpkg -i mysql-apt-config_0.8.33-1_all.deb
+sudo apt update
+sudo apt-get install mysql-server
+
 # Config db
-sudo -u postgres psql
-create database animation;
-create user admin with encrypted password 'secret';
-grant all privileges on database animation to admin;
+mysql -u root -p\'"$MYSQL_ROOT_PASSWORD"\' <<EOF
+CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
+GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%';
+FLUSH PRIVILEGES;
+CREATE DATABASE $MYSQL_DATABASE;
+CREATE DATABASE dev_$MYSQL_DATABASE;
+EOF
